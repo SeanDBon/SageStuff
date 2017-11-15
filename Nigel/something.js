95ac35ee9eb883014c86c9ecc9b4b7138069f283
SeanDBno <seanbonorden1@live.com>
2017-11-15 16:29:08 -0700

    hello2

--- Nigel/something.js
+++ Nigel/something.js
@@ -1,6 +1,11 @@
-function loop(){
-  for (let i = 1; i < 11; i++) {
-    console.log(i)
+function loop(i=1){
+  if (i > 10) {
+    console.zz
+    return i
   }
+  return loop(i + 1)
 }
 loop()
+
+
+arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
